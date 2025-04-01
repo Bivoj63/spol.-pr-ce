50a0e826a83e31aab63adaffac2fb684f11779f8
unknown <martin.friedel@infis.cz>
2025-04-01 08:33:05 +0200

    spol. práce

---
+++ skupin.py
@@ -0,0 +1,6 @@
+sportovníAktivita1 = input("co jste dělal za sport: ")
+sportovníAktivita2 = input("co jste dělal za druhý sport: ")
+čas1 = int(input("kolik minut první sport: "))
+čas2 = int(input("kolik minut druhý sport: "))
+celkovy_vydej = ((čas1 + čas2) * 5)
+print(celkovy_vydej)
